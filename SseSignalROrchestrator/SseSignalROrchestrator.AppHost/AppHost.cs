7f9833055e95ff22eea52f96c55763dd5bdbb48b
Paul Karam <paulkaram1989@hotmail.com>
2026-09-15 21:05:55 +0200

    third demo initial commit

--- SseSignalROrchestrator/SseSignalROrchestrator.AppHost/AppHost.cs
+++ SseSignalROrchestrator/SseSignalROrchestrator.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.SseApi>("sseapi");
+
+builder.AddProject<Projects.SignalRApi>("signalrapi");
+
 builder.Build().Run();
